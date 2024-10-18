06ccbf173333e62d33c43618327fb7a3ab4a2451
ibrahiim82 <yavuzyolcu82@gmail.com>
2024-10-18 22:13:05 +0300

    lastcommit

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { ListGroup } from "react-bootstrap";
+
+const TodoList = ({ todos }) => {
+  const deleteTodo = (id) => {};
+
+  return (
+    <div>
+      <h2 className="text-center text-secondary">Todos</h2>
+      <ListGroup className="w-50 d-flex mx-auto">
+        {todos.map((item) => (
+          <ListGroup.Item
+            variant="success"
+            className="m-3 text-capitalize rounded-5 d-flex justify-content-between"
+          >
+            <span>{item.text}</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="red"
+              onClick={() => deleteTodo(item.id)}
+              className="bi bi-trash-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
+            </svg>
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
+    </div>
+  );
+};
+
+export default TodoList;
